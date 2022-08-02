6a830d4c07ad4f152379dfe808099ef6aec1aeb1
mahfuzswaron <mahfuzswaron@gmail.com>
2022-08-02 15:14:03 +0600

    validateStringLength updated

--- classtasks/validateStringLength.py
+++ classtasks/validateStringLength.py
@@ -2,7 +2,8 @@ while True:
     str = input("Enter your text here: ")
     length = len(str)
     if(length < 10 or length > 20):
-        print("sorry, your inputed text is invalid")
+        print("sorry, your inputed text's length must be within 10 to 20")
+        print("your text is " , length, "character long" )
     else:
-        print("great!")
+        print("great! your text is ",length, "character long")
     

bf44c7de777c8ae4ac93d84c484315904021fc56
mahfuzswaron <mahfuzswaron@gmail.com>
2022-08-06 03:38:46 +0600

    dictionary changed

--- classtasks/validateStringLength.py
+++ classtasks/validateStringLength.py
@@ -2,8 +2,11 @@ while True:
     str = input("Enter your text here: ")
     length = len(str)
     if(length < 10 or length > 20):
-        print("sorry, your inputed text's length must be within 10 to 20")
-        print("your text is " , length, "character long" )
+        # print("sorry, your inputed text's length must be within 10 to 20")
+        # print("your text is " , length, "character long" )
+        #python format string
+        perfectStr = str[0:20]
+        print(perfectStr)
     else:
         print("great! your text is ",length, "character long")
     

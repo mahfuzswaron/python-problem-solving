d721beb3e6c91f248a318015b892df871bd6bd77
mahfuz swaron <mahfuzswaron@gmail.com>
2022-08-16 03:54:20 -0400

    validates string updated

--- classtasks/validateStringLength.py
+++ classtasks/validateStringLength.py
@@ -1,12 +1,19 @@
+vowels = ["a", 'e', "i", "o", "u"]
+def killVowels(word):
+    vLess=""
+    for w in word:
+        if(w not in vowels):
+            vLess = vLess + w
+    return vLess
+
 while True:
     str = input("Enter your text here: ")
     length = len(str)
-    if(length < 10 or length > 20):
-        # print("sorry, your inputed text's length must be within 10 to 20")
-        # print("your text is " , length, "character long" )
-        #python format string
-        perfectStr = str[0:20]
-        print(perfectStr)
+    if(length < 10):
+        print("Error: sorry, input msut be minimum 10 characters")
+    elif(length > 20):
+        print("Error: sorry maximum character limit [20] is over. You may try", str[0:20])
     else:
-        print("great! your text is ",length, "character long")
+        print("success: great! your text ( ", str , " ) is ", length, " character long" )
+        print("without vowels the text is :", killVowels(str))
     
